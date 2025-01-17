2f1a1ea584e8cdbf5f0789ea11174469635cefcd
Cauã Paduani <cauapaduani@gmail.com>
2025-01-17 02:46:33 -0300

    mudanças adicionadas

--- programs/smart_solana/src/lib.rs
+++ programs/smart_solana/src/lib.rs
@@ -11,26 +11,39 @@ pub mod smart_solana {
         Ok(())
     }
 
+    pub fn increment(ctx: Context<UpdateData>) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.data += 1;
+        Ok(())
+    }
 
-pub fn increment(ctx: Context<UpdateData>) -> Result<()> { let
-base_account = &mut ctx.accounts.base_account;
-base_account.data += 1;
-    Ok(())
-}
-pub fn get_data(ctx: Context<GetData>) -> Result<()> {
-    let base_account = &ctx.accounts.base_acount;
-    Ok(base_account.data)
-}
+    pub fn get_data(ctx: Context<GetData>) -> Result<u64> {
+        let base_account = &ctx.accounts.base_account;
+        Ok(base_account.data)
+    }
 }
 
 #[derive(Accounts)]
-pub struct Initialize <'Info> {
-    #[account(init, payer = user, space = 8+ 8)]
-    pub base_account: Account<'Info, BaseAccount>,
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 8)]
+    pub base_account: Account<'info, BaseAccount>,
     #[account(mut)]
-    pub user : Signer<'info>,
+    pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-pub struct UpdateData<'Info>
+pub struct UpdateData<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+#[derive(Accounts)]
+pub struct GetData<'info> {
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub data: u64,
+}   
